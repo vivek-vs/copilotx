3d13ae2569b58f5a91335708ee2961fcc149e791
Vivek Srivastava <vivek.srivastav@protonmail.com>
2023-04-13 09:20:11 +0530

    Initial Commit

---
+++ src/main/java/org/github/copilotx/copilotx/CopilotxApplication.java
@@ -0,0 +1,13 @@
+package org.github.copilotx.copilotx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/github/copilotx/copilotx/CopilotxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.copilotx.copilotx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
